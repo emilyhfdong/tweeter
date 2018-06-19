ea40b783ca6cf92728862756924dc67061e60ac8
Emily Dong <emilyhfdong@gmail.com>
2018-06-19 18:47:42 +0000

    character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".new-tweet textarea").on("keyup", function () {
+    let charsLeft = 140 - $(this).val().length;
+
+    $(this).siblings(".counter").text(charsLeft);
+    $(this).siblings(".counter").toggleClass("red-counter", charsLeft < 0);
+
+  });
+});
