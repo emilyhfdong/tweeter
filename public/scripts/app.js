0d35ef4a846f5c510e2e0f61794e837ea3100ad4
Emily Dong <emilyhfdong@gmail.com>
2018-06-20 18:14:31 +0000

    escape XSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,94 +1,47 @@
-// /*
-//  * Client-side JS logic goes here
-//  * jQuery is already loaded
-//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-//  */
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
+function escape(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 
 
-  function createTweetElement (dataObj) {
-    let $twe = $(`
-          <article class="tweet">
-            <header>
-              <div class="pic-and-name">
-                <img class="profile-pic" src=${dataObj.user.avatars.small} alt="profile picture">
-                <h2 class="name">${dataObj.user.name}</h2>
-              </div>
-              <p class="handle">${dataObj.user.handle}</p>
-            </header>
-            <div class="tweet-content">
-              <p>${dataObj.content.text}</p>
+function createTweetElement (dataObj) {
+  let $twe = $(`
+        <article class="tweet">
+          <header>
+            <div class="pic-and-name">
+              <img class="profile-pic" src=${dataObj.user.avatars.small} alt="profile picture">
+              <h2 class="name">${dataObj.user.name}</h2>
             </div>
-            <footer>
-              <p>${dataObj.created_at}</p>
-              <span class="icons"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i>
-              </span>
-            </footer>
-          </article>`);
+            <p class="handle">${dataObj.user.handle}</p>
+          </header>
+          <div class="tweet-content">
+            <p>${escape(dataObj.content.text)}</p>
+          </div>
+          <footer>
+            <p>${moment(dataObj.created_at).fromNow()}</p>
+            <span class="icons"><i class="fa fa-flag"></i><i class="fa fa-retweet"></i><i class="fa fa-heart"></i>
+            </span>
+          </footer>
+        </article>`);
 
-    return $twe
+  return $twe
 
-  }
+}
 
-  function renderTweets(dataArr) {
-    dataArr.reverse();
-    let $tweets = dataArr.map(obj => createTweetElement(obj));
-    $("#tweet-container").empty();
-    $(`#tweet-container`).append($tweets);
-  }
+function renderTweets(dataArr) {
+  dataArr.reverse();
+  let $tweets = dataArr.map(obj => createTweetElement(obj));
+  $("#tweet-container").empty();
+  $(`#tweet-container`).append($tweets);
+}
 
 $(document).ready(function() {
 
   function loadTweets() {
     $.ajax ({
-      url: 'tweets',
+      url: '/tweets',
       method: 'GET',
     }).done(function (dataJSON) {
       renderTweets(dataJSON);
@@ -99,8 +52,8 @@ $(document).ready(function() {
 
   $("#new-tweet-form").on("submit", function (event) {
     event.preventDefault();
-    $(this).siblings(".errors").find(".emptyError").remove();
-    $(this).siblings(".errors").find(".tooLong").remove();
+    $(this).siblings(".errors").empty();
+
 
     if($(".new-tweet textarea").val().trim() === "") {
       $emptyError = $("<p class='emptyError'>Tweet is empty!</p>")
