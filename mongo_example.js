7018fd91024d0c440997e2aba747c225db2a039c
Emily Dong <emilyhfdong@gmail.com>
2018-06-21 16:55:17 +0000

    switching to mongodb

---
+++ mongo_example.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // ==> We have a connection to the "tweeter" db,
+  //     starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  // ==> In typical node-callback style, any program
+  //     logic that needs to use the connection needs
+  //     to be invoked from within here.
+  //
+  // Another way to say: this is an "entry point" for
+  // a database-connected application!
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray(callback);
+  }
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+
+
+
+
+  // ==> At the end, we close the connection:
+});
