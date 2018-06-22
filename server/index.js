7610f024f33968b01b45fa9f06f1844fbec0d85f
Emily Dong <emilyhfdong@gmail.com>
2018-06-22 18:15:05 +0000

    sass

--- server/index.js
+++ server/index.js
@@ -5,10 +5,23 @@
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
+const nodeSassMiddleware = require('node-sass-middleware');
+const path = require('path');
+
 const app           = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
+console.log(__dirname)
+app.use(nodeSassMiddleware({
+    src: path.join(__dirname, '../src'),
+    dest: path.join(__dirname, '../public/styles'),
+    debug: true,
+    outputStyle: 'compressed'
+}));
+
 app.use(express.static("public"));
+
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 // const db = require("./lib/in-memory-db");
